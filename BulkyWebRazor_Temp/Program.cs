0e73747aae76d7e1b47e67aaf69bf18560f43f35
Ramez <ramezemad44@gmail.com>
2025-05-21 02:19:19 +0300

    Category CRUD Operation

--- BulkyWebRazor_Temp/Program.cs
+++ BulkyWebRazor_Temp/Program.cs
@@ -1,7 +1,4 @@
-using BulkyWeb.Data;
-using Microsoft.EntityFrameworkCore;
-
-namespace BulkyWeb
+namespace BulkyWebRazor_Temp
 {
     public class Program
     {
@@ -10,21 +7,14 @@ namespace BulkyWeb
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-            builder.Services.AddControllersWithViews();
-
-            builder.Services.AddDbContext<ApplicationDbContext>(opt =>
-                opt.UseSqlServer(
-                    builder.Configuration.GetConnectionString("DefaultConnection")
-                    )
-                );
-
+            builder.Services.AddRazorPages();
 
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
@@ -36,9 +26,7 @@ namespace BulkyWeb
 
             app.UseAuthorization();
 
-            app.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+            app.MapRazorPages();
 
             app.Run();
         }
